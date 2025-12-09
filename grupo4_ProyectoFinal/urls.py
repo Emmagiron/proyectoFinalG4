82e356a3b05f532bbb8805c9c425f9ce43715c12
Reridot <167099238+Reridot@users.noreply.github.com>
2025-12-08 22:40:21 -0300

    NO CORRESPONDE SUBIR EL ENV
    Se elimino el entorno que se encontraba dentro del proyecto. QUEDA SUJETO A REVISION EXAUSTIVA.

--- grupo4_ProyectoFinal/urls.py
+++ grupo4_ProyectoFinal/urls.py
@@ -1,5 +1,5 @@
-""" 
-URL configuration for grupo4_ProyectoFinal project.
+"""
+URL configuration for Grupo4_ProyectoFinal project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.2/topics/http/urls/
@@ -14,18 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# Archivo: Grupo4_ProyectoFinal/urls.py
 
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import inicio
 
 urlpatterns = [
+    # 1. RUTA CLAVE: Asigna la raíz (URL vacía: '') al blog
+    path('', include('blog.urls')), 
+    
+    # 2. Rutas existentes:
     path('admin/', admin.site.urls),
-    #Pagina de inicio www.mipagina.com/->
-    path('', inicio, name='inicio'),
-    #path('/articulos', ir al urls.py de la aplicacion "articulos")
-    path('articulos', include('apps.articulos.urls')),
-    path('autenticacion/', include('apps.autenticacion.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('blog/', include('blog.urls')),
+    path('cuentas/', include('blog.autenticacion.urls')), # Si la tienes
+
+    # RUTA PARA IMÁGENES
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
