94aca85672a63c8089cd72fa4f5750076a03470a
Reridot <167099238+Reridot@users.noreply.github.com>
2025-12-10 09:41:07 -0300

    Cambios minimos

--- grupo4_ProyectoFinal/settings/production.py
+++ grupo4_ProyectoFinal/settings/production.py
@@ -3,7 +3,9 @@ from .base import *
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    # Aqui va el dominio de "python anywhere" de la producción
+]
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
