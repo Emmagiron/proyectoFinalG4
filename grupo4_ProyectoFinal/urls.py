9e5a0dd6f20fc1f594379e60a7639d2999cba7cd
celestezapata0007 <coloristaybarber07@gmail.com>
2025-12-10 22:26:23 -0300

    feat: Configuración final de deploy (Procfile y variables de entorno)

--- grupo4_ProyectoFinal/urls.py
+++ grupo4_ProyectoFinal/urls.py
@@ -5,12 +5,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# Importamos la vista 'inicio' si existe
+
 try:
-    from .views import inicio # Intentamos importar desde la carpeta actual
+    from .views import inicio 
 except ImportError:
-    # Si la vista de inicio no está en .views, la ponemos como pass para evitar error.
-    # Si no existe, es mejor que uses la ruta del blog como inicio.
+    
     pass 
 
 
@@ -19,8 +18,7 @@ urlpatterns = [
     # Rutas Principales
     path('admin/', admin.site.urls),
     
-    # RUTA DE INICIO (Priorizamos la del blog o la de inicio)
-    # Si quieres que el Blog sea la página de inicio (www.mipagina.com/)
+    
     path('', include('blog.urls')), 
     
     # RUTA DE AUTENTICACIÓN
@@ -29,11 +27,7 @@ urlpatterns = [
     # RUTAS DE OTRAS APLICACIONES
     path('articulos/', include('apps.articulos.urls')),
     
-    # Si la ruta 'blog/' se va a usar para otras cosas, la mantenemos, si no, es redundante
-    # path('blog/', include('blog.urls')), 
-    
-    # Si existe una ruta de autenticación diferente
-    path('cuentas/', include('blog.autenticacion.urls')), 
+
 ]
 
 # Configuración para archivos media (imágenes)
