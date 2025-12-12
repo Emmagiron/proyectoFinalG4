8ba59b0ec6940115d14c017c81fa442e42b1d080
Emmagiron <desgracia19952@gmail.com>
2025-12-12 18:16:58 -0300

    acerca de funcionando
    tenia un problema en el for en django no lo toma como lista dentro y confunde el bloque esperando un endblock en vez de un end for en fin eso y ademas habia una problema en las funciones pero solucionado

--- grupo4_ProyectoFinal/views.py
+++ grupo4_ProyectoFinal/views.py
@@ -4,8 +4,11 @@ from django.shortcuts import render
     
 
 def acerca_de(request):
-    """
-    Vista que renderiza la plantilla 'Acerca de'.
-    Aquí no se necesita lógica de base de datos, solo mostrar el HTML.
-    """
-    return render(request, 'acerca_de.html', {}) 
+    miembros = [
+        {'nombre': 'Román Huel', 'rol': 'Líder Técnico y Desarrollador Backend', 'contribucion': 'Arquitectura de modelos, lógica de filtrado, implementación de vistas CRUD.'},
+        {'nombre': 'Valentina Sapag', 'rol': 'Diseñadora Frontend y Administradora', 'contribucion': 'Maquetación HTML/CSS, adaptación responsive, implementación de Comentarios y Registro.'},
+        {'nombre': 'Emmanuel Girón', 'rol': 'Diseñador Frontend y Administrador', 'contribucion': 'Maquetación HTML/CSS, adaptación responsive, implementación de Comentarios y Registro.'},
+        {'nombre': 'Maria Celeste Zapata', 'rol': 'Diseñadora Frontend y Administradora', 'contribucion': 'Maquetación HTML/CSS, adaptación responsive, implementación de Comentarios y Registro.'}
+    ]
+
+    return render(request, 'acerca_de.html', {'miembros': miembros})
